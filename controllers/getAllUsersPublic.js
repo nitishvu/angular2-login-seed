184a75b8f08c72204cc582b6a001d2008f6d4665
Dom Farolino <domfarolino@gmail.com>
2016-03-27 17:58:09 -0400

    Got rid of diagnostic console.log()

--- controllers/getAllUsersPublic.js
+++ controllers/getAllUsersPublic.js
@@ -4,15 +4,6 @@ var User = require('../models').User;
 module.exports = Promise.method(function getAllUsersPublic(offset, limit, desc) {
   offset*=1;
   limit*=1;
-  console.log(desc);
-  console.log(desc);
-  console.log(desc);
-  console.log(desc);
-  console.log(desc);
-  console.log(desc);
-  console.log(desc);
-  console.log(desc);
-  console.log(desc);
   var idOrder = (desc) ? ['id', 'DESC'] : ['id', 'ASC'];
   return User.findAll({order: [idOrder], offset: offset, limit:limit}).then(function(users) {
     for (var i = 0; i < users.length; ++i) {
@@ -21,7 +12,7 @@ module.exports = Promise.method(function getAllUsersPublic(offset, limit, desc) 
         "username": users[i].username,
         "profile_picture": users[i].profile_picture,
         "last_active": users[i].last_active
-      };
+      }; 
     }
     console.log(users);
     return users;
