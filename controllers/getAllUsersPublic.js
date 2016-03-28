436c40dedaafa2fc8fe27e451ce1c34a5eeb75b2
Dom Farolino <domfarolino@gmail.com>
2016-03-27 21:22:55 -0400

    White space cleanup

--- controllers/getAllUsersPublic.js
+++ controllers/getAllUsersPublic.js
@@ -12,7 +12,7 @@ module.exports = Promise.method(function getAllUsersPublic(offset, limit, desc) 
         "username": users[i].username,
         "profile_picture": users[i].profile_picture,
         "last_active": users[i].last_active
-      }; 
+      };
     }
     console.log(users);
     return users;
