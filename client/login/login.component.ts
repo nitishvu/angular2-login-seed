426e81a78d3809d540ec6118c55cd0e0e2450a3a
Dom Farolino <domfarolino@gmail.com>
2016-04-04 21:42:11 -0400

    More material2 upgrades - login screen

--- client/login/login.component.ts
+++ client/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from 'angular2/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MdButton } from '@angular2-material/button';
 
 @Component({
     selector: 'login',
     templateUrl: 'client/login/login.component.html',
-    styleUrls: ['client/login/login.component.css']
+    styleUrls: ['client/login/login.component.css'],
+    directives: [MD_CARD_DIRECTIVES, MdButton]
 })
 
 export class LoginComponent{
