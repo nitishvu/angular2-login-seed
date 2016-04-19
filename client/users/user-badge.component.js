d646ae638271852b687e36d3ce2f89a133184d8a
Dom Farolino <domfarolino@gmail.com>
2016-04-18 20:48:31 -0400

    Added md-card to replace paper-card in users component

--- client/users/user-badge.component.js
+++ client/users/user-badge.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', '@angular2-material/card'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, card_1;
     var UserBadgeComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (card_1_1) {
+                card_1 = card_1_1;
             }],
         execute: function() {
             UserBadgeComponent = (function () {
@@ -38,7 +41,8 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                     core_1.Component({
                         selector: 'user-badge',
                         templateUrl: 'client/users/user-badge.component.html',
-                        styleUrls: ['client/users/user-badge.component.css']
+                        styleUrls: ['client/users/user-badge.component.css'],
+                        directives: [card_1.MD_CARD_DIRECTIVES]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], UserBadgeComponent);
--- client/users/user-badge.component.ts
+++ client/users/user-badge.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from 'angular2/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 
 @Component({
     selector: 'user-badge',
     templateUrl: 'client/users/user-badge.component.html',
-    styleUrls: ['client/users/user-badge.component.css']
+    styleUrls: ['client/users/user-badge.component.css'],
+    directives: [MD_CARD_DIRECTIVES]
 })
 
 export class UserBadgeComponent {
