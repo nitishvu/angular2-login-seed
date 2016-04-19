0a2452dd09bd6d68ca0eb7ffa1d54e9082c2fcd4
Dom Farolino <domfarolino@gmail.com>
2016-04-18 21:57:52 -0400

    docs(passport): updated passport with much needed documentation, README doc for adding providers will follow shortly

--- config/passport.js
+++ config/passport.js
@@ -12,6 +12,11 @@ var GoogleStrategy  = require('passport-google-oauth2').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
 var requestPromise  = require('request-promise');
  
+/**
+ * These can be (may be in the future) more complex
+ * if need be. Depends on how you are handling authentication
+ * and serialization
+ */
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -20,6 +25,11 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
+/**
+ * PassportJS Google strategy specifics will just below here
+ * Assumes you've inputted your OAuth service credentials in the
+ * /config/default.json or production file as necessary.
+ */
 passport.use(
 
   new GoogleStrategy({
@@ -60,6 +70,11 @@ passport.use(
 
 ); // end passport.use()
 
+/**
+ * PassportJS Twitter strategy specifics will just below here
+ * Assumes you've inputted your OAuth service credentials in the
+ * /config/default.json or production file as necessary.
+ */
 passport.use(new TwitterStrategy({
     consumerKey: config.get('oauthCredentials.twitter.id'),
     consumerSecret: config.get('oauthCredentials.twitter.secret'),
@@ -98,8 +113,17 @@ passport.use(new TwitterStrategy({
 
 
 /**
- * Used to findOrCreateUser for any provider. Utilizes the buildUpdatedAttributes
- * so we can either create or update a user with updated attributes
+ * Used to findOrCreateUser for any provider.
+ * Utilizes the buildUpdatedAttributes function so we can
+ * either create or update a user with updated attributes
+ *
+ * @param {String} provider (Google, Twitter, Facebook, LinkedIn, etc)
+ * @param {String} social_id unique identification of the user with the OAuth provider
+ * @param {Object} profile some user information object returned by the OAuth provider
+ * @param {Object} attributes partially complete object of user attributes only containing
+ *   authentication information (access token, etc).
+ * 
+ * @return {User} created or updated user
  */
 function findOrCreateUser(provider, social_id, profile, attributes) {
   return User.findOne({where: {provider: provider, social_id: social_id}}).then(function(user) {
@@ -135,12 +159,32 @@ function findOrCreateUser(provider, social_id, profile, attributes) {
 }
 
 /**
- * Utility function to build updated list of attributes that
- * a logged in user will need to be updated with
- * Works for any provider
+ * Below are utility functions to build user attribute objects based
+ * on whether they are being logged in and UPDATED, or logged in and
+ * CREATED. Basically the difference is that when we log in an already
+ * existing user we only update authentication information (access token, etc)
+ * and the profile picture. This is subject to change.
+ */
+
+/**
+ * UPDATE user attributes
+ * Build updated list of attributes that a logged in user will need
+ * to be UPDATED with (profile_picture, last active, OAuth authentication information).
+ * Works for any provider* though a more maintainable implementation
+ * would be ideal. Issue is that each provider packages the same
+ * information slightly differently so we must deal with that somewhere
+ *
+ * @param {String} provider (Google, Twitter, Facebook, LinkedIn, etc)
+ * @param {String} social_id unique identification of the user with the OAuth provider
+ * @param {Object} profile some user information object returned by the OAuth provider
+ * @param {Object} attributes partially complete object of user attributes we will finish
+ *   filling out here
+ * 
+ * @return {Object} updatedUserAttributes object containing all the attributes needed to update
+ *   a user in the database with
  */
 function buildUpdatedAttributes(provider, social_id, profile, user, attributes) {
-  var updatedUserAttributes;
+  var updatedUserAttributes = {};
   if(provider == 'google') {
     updatedUserAttributes = {
       profile_picture: profile.photos[0].value.split("?")[0],
@@ -162,14 +206,23 @@ function buildUpdatedAttributes(provider, social_id, profile, user, attributes) 
 }
 
 /**
- * Utility function to build complete list of attributes that
- * a logged in user will need to be created with
- * Works for any provider
+ * CREATE all user attributes
+ * Build list of attributes that a newly created user will need
+ * to to have associated with them in the database. See /models/user.js
+ * 
+ * @param {String} provider (Google, Twitter, Facebook, LinkedIn, etc)
+ * @param {String} social_id unique identification of the user with the OAuth provider
+ * @param {Object} profile some user information object returned by the OAuth provider
+ * @param {Object} attributes partially complete object of user attributes we will
+ *   completely fill in here
+ * 
+ * @return {Object} completeUserAttributes object containing all the attributes needed to add
+ *   a user to the database
  */
 function buildCompleteAttributes(provider, social_id, profile, user, attributes) {
-  var updatedUserAttributes;
+  var completeUserAttributes;
   if(provider == 'google') {
-    updatedUserAttributes = {
+    completeUserAttributes = {
       social_id: social_id,
       name: profile.displayName,
       username: profile.email.split("@")[0],
@@ -181,7 +234,7 @@ function buildCompleteAttributes(provider, social_id, profile, user, attributes)
       refresh_token: attributes.refresh_token
     };
   } else if(provider == 'twitter') {
-    updatedUserAttributes = {
+    completeUserAttributes = {
       social_id: social_id,
       name: profile.displayName,
       username: profile.username,
@@ -194,5 +247,5 @@ function buildCompleteAttributes(provider, social_id, profile, user, attributes)
     };
   }
   
-  return updatedUserAttributes;
+  return completeUserAttributes;
 }
