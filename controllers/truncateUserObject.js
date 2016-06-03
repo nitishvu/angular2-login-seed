4a82d914b0f81a766a567b37e74c2912bbe3edec
Dom Farolino <domfarolino@gmail.com>
2016-06-03 08:44:27 -0400

    docs(): update docs on user trimming controller

--- controllers/truncateUserObject.js
+++ controllers/truncateUserObject.js
@@ -2,7 +2,12 @@
  * Trim a full user object into a "public safe"
  * user object. The logic is housed here so when
  * the database grows more complex, we can manage
- * trimming a user in one place
+ * trimming a user in one place. This can even be
+ * role-based trimming based off of the user attributes
+ *
+ * @param {Object} full list of user attributes from database
+ * 
+ * @return {Object} "trimmed" user object containing publically accessible attributes
  */
 module.exports = function(userAttributes) {
   return {
